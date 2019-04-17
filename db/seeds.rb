8a770ce0bc70a19cc7afbc195a8e77e60aa159f9
Mira Tohme <mira@tohme.com>
2019-04-17 16:37:52 +0800

    restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning database'
+Restaurant.destroy_all
+restaurants_attribute = [
+  {
+    name: 'Ginette',
+    address: '5 ave des stars',
+    category: 'chinese'
+  },
+  {
+    name: 'Sushiko',
+    address: '15 robertson',
+    category: 'italian'
+  },
+  {
+    name: 'Meat The Fish',
+    address: 'sioufi village',
+    category: 'japanese'
+  },
+  {
+    name: 'Sultan',
+    address: '10 champs Elysee',
+    category: 'french'
+  },
+  {
+    name: 'Casablance',
+    address: '32, la corniche',
+    category: 'belgian'
+  }
+]
+
+Restaurant.create!(restaurants_attribute)
